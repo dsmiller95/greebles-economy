9387eb9cc51be8f6b100535e72f6f41a5d63af61
Dan Miller <dsmiller95@gmail.com>
2020-04-27 21:59:01 -0500

    added food spawner

---
+++ Assets/Gatherer Code/resource spawner/SpawnController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public int maximumConcurrentSpawned = 10;
+    public float spawnProbability = 0.5f;
+    public GameObject spawnPrefab;
+
+    public Vector3 spawnBoxSize = new Vector3(1, 1, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount < maximumConcurrentSpawned)
+        {
+            if(Random.value < spawnProbability * Time.deltaTime)
+            {
+                Instantiate(spawnPrefab, getRandomPosInBounds() + this.transform.position, Quaternion.identity, this.transform);
+            }
+        }
+    }
+
+    private Vector3 getRandomPosInBounds()
+    {
+        return this.spawnBoxSize * (Random.value - 0.5f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0.8f, 0.2f, 0.2f, 0.5f);
+        Gizmos.DrawCube(transform.position, spawnBoxSize);
+    }
+}
