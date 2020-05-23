d7f8d67cadc6ce4cb163df575e8fbeabd13d2fcb
Dan Miller <dsmiller95@gmail.com>
2020-05-23 12:54:17 -0500

    Added new utility function and test project to test it

--- Assets/Utilities.cs
+++ Assets/Utilities.cs
@@ -11,4 +11,32 @@ public static class Utilities
         serializer = serializer ?? (s => s.ToString());
         return dictionary.Select(entry => $"{Enum.GetName(typeof(ResourceType), entry.Key)}: {serializer(entry.Value)}").Aggregate((agg, current) => agg + "; " + current);
     }
+
+    /// <summary>
+    /// create or destroy objects based on how many are needed in the list
+    /// </summary>
+    /// <param name="neededObjects"></param>
+    /// <param name="objectList">the list of objects to modify</param>
+    /// <param name="objectGenerator">a generator function to create new objects</param>
+    /// <param name="objectDestroyer">a function to be called when an object is removed</param>
+    public static IList<T> EnsureAllObjectsCreated<T>(int neededObjects, IList<T> objectList, Func<T> objectGenerator, Action<T> objectDestroyer)
+    {
+        if (objectList.Count > neededObjects)
+        {
+            foreach (var objectToDelete in objectList.Skip(neededObjects))
+            {
+                objectDestroyer(objectToDelete);
+            }
+            return objectList.Take(neededObjects).ToList();
+        }
+
+        if (objectList.Count < neededObjects)
+        {
+            for (int dotToAdd = objectList.Count; dotToAdd < neededObjects; dotToAdd++)
+            {
+                objectList.Add(objectGenerator());
+            }
+        }
+        return objectList;
+    }
 }
---
+++ UnitTests/UnitTests/UtilitiesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Utility_EnsureAllObjectsCreated
+    {
+        class TestClass
+        {
+            public TestClass(int value = 0)
+            {
+                this.myValue = value;
+            }
+            public int myValue;
+        }
+        [TestMethod]
+        public void WhenCreatingObjectsInEmptyList()
+        {
+            var beginningList = new List<TestClass>();
+            var resultList = Utilities.EnsureAllObjectsCreated(10, beginningList, () => new TestClass(2), obj => obj.myValue = 10);
+
+            Assert.AreEqual(10, resultList.Count);
+            Assert.IsTrue(resultList.All(x => x.myValue == 2));
+        }
+        [TestMethod]
+        public void WhenRemovingObjectInFullList_ShouldRemove()
+        {
+            var beginningList = new List<TestClass>() { new TestClass(3), new TestClass(3), new TestClass(3) };
+            var beginningListCopy = beginningList.Select(x => x).ToList();
+            var resultList = Utilities.EnsureAllObjectsCreated(1, beginningList, () => new TestClass(2), obj => obj.myValue = 10);
+
+            Assert.AreEqual(1, resultList.Count);
+            Assert.IsTrue(resultList.All(x => x.myValue == 3));
+            Assert.AreEqual(10, beginningListCopy[1].myValue);
+            Assert.AreEqual(10, beginningListCopy[2].myValue);
+        }
+    }
+}
