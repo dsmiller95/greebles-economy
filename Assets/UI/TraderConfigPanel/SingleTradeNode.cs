db04c2c597b095fdc0a84d03984d2663a5cd1280
Dan Miller <dsmiller95@gmail.com>
2020-06-20 17:32:59 -0500

    Added expandable dropdown

--- Assets/UI/TraderConfigPanel/SingleTradeNode.cs
+++ Assets/UI/TraderConfigPanel/SingleTradeNode.cs
@@ -10,14 +10,41 @@ namespace Assets.UI.TraderConfigPanel
         public TradeNode tradeNode;
         public Text description;
 
+        [Header("Button Expander")]
+        public Button expandButton;
+        public Text expandButtonText;
+
+        [Header("Expander Items")]
         public GameObject singleResourceTradePrefab;
         public GameObject singleResourceTradeContainer;
+
+        private bool expanded = false;
         // Start is called before the first frame update
         void Start()
         {
             description.text = $"Target: {tradeNode.targetMarket.name}";
 
-            var tradeNodes = tradeNode.trades.Select(node => CreateSingleResourceTrade(node)).ToList();
+            //var tradeNodes = tradeNode.trades.Select(node => CreateSingleResourceTrade(node)).ToList();
+            expandButton.onClick.AddListener(this.ExpandButtonClicked);
+        }
+
+        private void ExpandButtonClicked()
+        {
+            if (expanded)
+            {
+                foreach(Transform child in singleResourceTradeContainer.transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            } else
+            {
+                foreach(var node in tradeNode.trades)
+                {
+                    CreateSingleResourceTrade(node);
+                }
+            }
+            expanded = !expanded;
+            expandButtonText.text = expanded ? "^" : "v";
         }
 
         private GameObject CreateSingleResourceTrade(ResourceTrade trade)
